d40770925d5aa683ca9ca9f5035f5a1f18a7b531
Joanne Cheng <joanne_cheng@chiefgroup.com.hk>
2021-02-16 19:53:56 +0800

    Init project

---
+++ AlbumList/AlbumList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlbumList
+//
+//  Created by Joanne Cheng on 16/2/2021.
+//  Copyright © 2021 Joanne Cheng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
